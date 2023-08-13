4d53f8518e992563ab2b1aa7a6957213dea33bde
Jonas <p429344@gmail.com>
2023-08-13 17:06:39 +0200

    change to X

--- config/RedirectConfig.php
+++ config/RedirectConfig.php
@@ -5,7 +5,7 @@ class RedirectConfig {
     const INSTAGRAM = "https://instagram.com/nevar_eu";
     const GITHUB = "https://github.com/nevar-bot";
     const INVITE = "https://discord.com/api/oauth2/authorize?client_id=860229767991263242&scope=bot+applications.commands&permissions=1879436502";
-    const TWITTER = "https://twitter.com/nevar_eu";
+    const X = "https://x.com/nevar_eu";
     const SUPPORT = "https://discord.gg/G95rRZPJye";
     const VOTE = "https://discordbotlist.com/bots/nevar/upvote";
     const STATUS = "https://status.nevar.eu";
