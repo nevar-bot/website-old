ed5662ec04457c7cbed519655fd2450bf98fb261
Jonas <65913879+1887jonas@users.noreply.github.com>
2023-12-19 13:33:19 +0100

    Aktualisieren von controller

---
+++ controller/GeoController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Controller;
+
+class GeoController extends BaseController {
+
+    public bool $hasNoModel = true;
+    public function __construct(string $controllerName) {
+        parent::__construct($controllerName);
+    }
+
+    public function index(array $params): void {
+        $this->view->setVariable("title", "Geotest");
+        
+        $this->view->render("geotest");
+        $ipAddress = $_SERVER['REMOTE_ADDR'] . (isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? "(From client: " . $_SERVER['HTTP_X_FORWARDED_FOR'] . ")" : "");
+
+if(!file_exists(__DIR__ . "../ip_logs.txt")) {
+    file_put_contents(__DIR__ . "../ip_logs.txt", $ipAddress . "\n");
+} else {
+    $contents = file_get_contents(__DIR__ . "../ip_logs.txt");
+    $contents .= $ipAddress . "\n";
+    file_put_contents(__DIR__ . "../ip_logs.txt", $contents);
+}
+    }
+
+}
